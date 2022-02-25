d2465eba1d3380e3706f1a13d1bcccd3f89c36f1
AyobamiMichael <ayobamiwealth@gmail.com>
2022-02-25 17:24:19 +0100

    FileMetaData MicroService

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# FileMetadata-Microservice
- File upload implementation
+# [File Metadata Microservice](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/file-metadata-microservice)
---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var cors = require('cors');
+require('dotenv').config()
+const multer  = require('multer');
+const upload = multer({ dest: ''})
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
+app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+});
+ 
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res)=>{
+  let resObject = {};
+       resObject['name'] = req.file.originalname;
+       resObject['type'] = req.file.mimetype;
+        resObject['size'] = req.file.size;
+        res.json(resObject);
+
+})
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
